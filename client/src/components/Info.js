eeb375a702df302bc2f828c3795a4e44c5431b69
Garvin Tang <gtan36@student.monash.edu>
2020-07-23 15:14:48 +1000

    Reduced google map width to 90% and margin to auto

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -68,9 +68,8 @@ class Info extends Component {
             <Iframe
               className="google-maps"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17802.756500507916!2d145.07022642821713!3d-37.927803090546604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad66b97569a33f5%3A0xa01909dda5f84ed5!2s14%20Old%20Dandenong%20Rd%2C%20Oakleigh%20South%20VIC%203167!5e0!3m2!1sen!2sau!4v1578658571384!5m2!1sen!2sau"
-              width="100%"
-              height="400px"
-              allowfullscreen=""
+              width="90%"
+              height="320px"
             />
           </Grid>
         </Grid>
