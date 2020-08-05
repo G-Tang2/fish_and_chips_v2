d169ed7bc453be7b9b0e85c24dd799efb1c81206
Garvin Tang <gtan36@student.monash.edu>
2020-08-06 00:24:52 +1000

    Changed button default font-family to Segoe UI

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,15 @@ import Info from "./components/Info";
 import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
 import "./stylesheets/main.scss";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  MuiButton: {
+    root: {
+      fontFamily: ["Segoe UI", "Roboto", "Helvetica", "Arial", "sans-serif"].join(","),
+    },
+  },
+});
 
 class App extends Component {
   constructor(props) {
@@ -20,22 +29,24 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Header heading="Oakleigh South Fish and Chips" ref={this.headerRef} menuRef={this.menuRef} infoRef={this.infoRef} contactRef={this.contactRef} />
-        <Hero />
-        <FrontPageDescription description="Freshly cooked in 100% cholesterol free vegetable oil" />
-        <div className="menu" ref={this.menuRef}>
-          <Menu heading="Menu" headerRef={this.headerRef} />
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header heading="Oakleigh South Fish and Chips" ref={this.headerRef} menuRef={this.menuRef} infoRef={this.infoRef} contactRef={this.contactRef} />
+          <Hero />
+          <FrontPageDescription description="Freshly cooked in 100% cholesterol free vegetable oil" />
+          <div className="menu" ref={this.menuRef}>
+            <Menu heading="Menu" headerRef={this.headerRef} />
+          </div>
+          <Images />
+          <div ref={this.infoRef}>
+            <Info heading="Info" address="14 Old Dandenong Road Oakleigh South 3167" phoneNo="9570 2222" openHrHeading="Opening Hours" />
+          </div>
+          <div className="contact" ref={this.contactRef}>
+            <ContactForm />
+          </div>
+          <Footer />
         </div>
-        <Images />
-        <div ref={this.infoRef}>
-          <Info heading="Info" address="14 Old Dandenong Road Oakleigh South 3167" phoneNo="9570 2222" openHrHeading="Opening Hours" />
-        </div>
-        <div className="contact" ref={this.contactRef}>
-          <ContactForm />
-        </div>
-        <Footer />
-      </div>
+      </ThemeProvider>
     );
   }
 }
--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
 import img1 from "../images/img1.jpg";
 import img2 from "../images/img2.jpg";
 import img3 from "../images/img3.jpg";
@@ -13,17 +11,6 @@ class Images extends Component {
         <img className="food-image" src={img2} />
         <img className="food-image" src={img3} />
       </div>
-      //   <GridList cols={3}>
-      //     <GridListTile cols={1}>
-      //       <img src={img1} />
-      //     </GridListTile>
-      //     <GridListTile cols={1}>
-      //       <img src={img2} />
-      //     </GridListTile>
-      //     <GridListTile cols={1}>
-      //       <img src={img3} />
-      //     </GridListTile>
-      //   </GridList>
     );
   }
 }
